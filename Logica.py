c67937a334e96636dbf9e01854695f6286af56df
ionatan <ionatan@gmail.com>
2020-09-12 18:03:59 -0300

    creo que reimplemente con algunas simplificaciones hasta la clase parentesis

--- Logica.py
+++ Logica.py
@@ -1,64 +1,3 @@
-
-class Expresion:
-
-    name = "Expresion"
-    operadores = [Parentesis] #, SiSoloSi, Implica, Proposicion]
-
-    def __init__ (self,texto,padre):
-        assert texto
-        assert type(texto) == str
-        self.texto = texto
-        self.padre = padre
-        self.elementosTemporales = [texto]
-        self.textoRemanente = texto
-        self.mensajesError = []
-        self.operacion = None
-        self.subexpresiones = None
-        self.validado = False
-        self.procesar()
-        if self.mensajesError:
-            for mensaje in self.mensajesError:
-                mensaje.reportar()
-        
-    def crearsubexpresion (cls,elementosTemporales,padre):
-        "Transforma un subconjunto de elementos a medio procesar en el texto correspodiente para crear una subexpresion nueva."
-        texto = cls.elementosAtexto (elementosTemporales)
-        assert texto
-        return Expresion(texto,padre)
-
-    def reemplazarCaracteres (cls,texto):
-        "Reemplaza los caracteres de un texto asumiento que el texto fue ingresado por el usuario y hay diferentes equivalencias para los mismos simbolos"
-        for operador in cls.operadores:
-            texto = operador.reemplazarCaracteres(texto)
-        return texto
-
-    def expresionInicial (cls,texto):
-        "Es el punto de entreda al codigo, toma el texto del usuario e inicia el proceso."
-        texto = cls.reemplazarCaracteres(texto)
-        expresion = Expresion(texto,False)
-        if expresion.validado:
-            print ("La expresion: " + expresion.texto + " ha pasado el proceso de procesamiento y validacion sintactica")
-        else:
-            print ("La expresion: " + expresion.texto + " no ha pasado el proceso de procesamiento y validacion sintactica")
-
-    def totext(self):
-        pass # No me queda claro para que la querria. 
-        
-    def procesar(self):
-        pass # Revisar implementacion cuando termine de reescribir los demas elementos. 
-        
-class MensajeError:
-    "Clase que se crea cuando aparece un error en una expresión para explicarlo."
-    def __init__(self,contexto,operador,mensaje):
-        self.contexto = contexto
-        self.operador = operador
-        self.mensaje = mensaje
-
-    def reportar(self):
-        "Genera el texto que explica el error."
-        return "En el la expresion " + self.contexto.texto + " se encontro un error al procesar un operador de tipo " + self.operador.name + " que reporto el siguiente mensaje: " + self.mensaje
-
-
 class Operador:
     "Clase correspondiente a un operador generico donde se definen las funciones comunes a todos."
     name = None
@@ -149,38 +88,15 @@ class Parentesis(Operador):
         - Que los parentesis tengan contenido
         - Que no haya mas de un parentesis sin que quede texto fuera
         
-        """
-        
+        """        
         pares = cls.buscarPares(expresion)
         if pares == -1:
-            return
+            return # Se vuelve sin aplicar nada porque hubo un error en la sintaxis de los parentesis.
         else:
-            if pares:
-            else:
-                expresion.ele
-
-        if len(aperturas) == 0: # No se encontraron parentesis
-            expresion.elementos.append(expresion.texto) 
-        else:
-            pares = []
-            while len(aperturas) > 0:
-                if aperturas[0] < cierres[0]:
-                    if len(aperturas) == 1:
-                        pares = pares + [[aperturas[0],cierres[0]]]
-                        aperturas.pop(0)
-                        cierres.pop(0)
-                    else:
-                        if aperturas[1] < cierres[0]:
-                            aperturas.pop(1)
-                            cierres.pop(0) 
-                        else:
-                            pares = pares + [[aperturas[0],cierres[0]]]
-                            aperturas.pop(0)
-                            cierres.pop(0)
-                else:
-                    error = MensajeError(expresion,Parentesis,"En la expresion se encontro un parentesis de apertura antes que uno de cierre.")
-                    expresion.mensajesError.append(error)
-                    return
+            if len(pares) == 1:
+                if pares[0][0] == 0 and pares[0][1] == len(expresion.texto):
+                    expresion.texto = expresion.texto[1:-1]
+                    return # Se quito los parentesis externos y se acaba el chequeo (elementosTemporales queda vacio)
             remanente = expresion.texto
             for par in pares:
                 pre = expresion.texto[len(expresion.texto)-len(remanente):par[0]]
@@ -188,12 +104,6 @@ class Parentesis(Operador):
                     expresion.elementos.append(expresion.texto[len(expresion.texto)-len(remanente):par[0]])
                 contenido = expresion.texto[par[0]+1:par[1]]
                 if contenido:
-                    if expresion.elementos:
-                        if not type(expresion.elementos[-1]) == str: #TODO Revisar si esta bien este chequeo, sobre todo el mensaje de error. Porque no tiene sentido que haya una proposicion creo. El parentesis es lo primero que se chequea a partir del texto
-                            if expresion.elementos[-1].name == Expresion.name:
-                                error = MensajeError(expresion,Parentesis,"En la expresion se encontro un parentesis consecutivo a otro o consecutivo a una expresion proposicional sin operador de por medio.")
-                                expresion.mensajesError.append(error)
-                                return    
                     expresion.elementos.append(Expresion(contenido,expresion))
                 else:
                     error = MensajeError(expresion,Parentesis,"En la expresion se encontro un parentesis de apertura y cierre sin contenido.")
@@ -202,9 +112,71 @@ class Parentesis(Operador):
                 remanente = expresion.texto[par[1]+1:]
             if remanente:
                 expresion.elementos.append(remanente)
-            
-            if len(expresion.elementos) == 1: # Si solo hay un elemento deberia ser una expresion y no hay mas que validar
-                expresion.continuarValidacion = False
-            else:
-                #TODO pasar el to texto a cuando se arma, tipo constructor no en el init. Hacer que solo se reciba texto
-                pass
+
+
+class Expresion:
+
+    name = "Expresion"
+    operadores = [Parentesis] #, SiSoloSi, Implica, Proposicion]
+
+    def __init__ (self,texto,padre):
+        assert texto
+        assert type(texto) == str
+        self.texto = texto
+        self.padre = padre
+        self.elementosTemporales = [texto]
+        self.textoRemanente = texto
+        self.mensajesError = []
+        self.operacion = None
+        self.subexpresiones = None
+        self.validado = False
+        self.continuarvalidacion = True
+        self.procesar()
+        if self.mensajesError:
+            for mensaje in self.mensajesError:
+                mensaje.reportar()
+        
+    @staticmethod
+    def crearsubexpresion (cls,elementosTemporales,padre):
+        "Transforma un subconjunto de elementos a medio procesar en el texto correspodiente para crear una subexpresion nueva."
+        texto = cls.elementosAtexto(elementosTemporales)
+        assert texto
+        return Expresion(texto,padre)
+
+    @staticmethod
+    def reemplazarCaracteres (cls,texto):
+        "Reemplaza los caracteres de un texto asumiento que el texto fue ingresado por el usuario y hay diferentes equivalencias para los mismos simbolos"
+        for operador in cls.operadores:
+            texto = operador.reemplazarCaracteres(texto)
+        return texto
+
+    @staticmethod
+    def expresionInicial (cls,texto):
+        "Es el punto de entreda al codigo, toma el texto del usuario e inicia el proceso."
+        texto = cls.reemplazarCaracteres(texto)
+        expresion = Expresion(texto,False)
+        if expresion.validado:
+            print ("La expresion: " + expresion.texto + " ha pasado el proceso de procesamiento y validacion sintactica")
+        else:
+            print ("La expresion: " + expresion.texto + " no ha pasado el proceso de procesamiento y validacion sintactica")
+
+    def totext(self):
+        pass # No me queda claro para que la querria. 
+        
+    def procesar(self):
+        pass # Revisar implementacion cuando termine de reescribir los demas elementos. 
+        
+class MensajeError:
+    "Clase que se crea cuando aparece un error en una expresión para explicarlo."
+    def __init__(self,contexto,operador,mensaje):
+        self.contexto = contexto
+        self.contexto.continuarvalidacion = False
+        self.operador = operador
+        self.mensaje = mensaje
+        self.contexto.mensajesError.append(self)
+
+    def reportar(self):
+        "Genera el texto que explica el error."
+        return "En el la expresion " + self.contexto.texto + " se encontro un error al procesar un operador de tipo " + self.operador.name + " que reporto el siguiente mensaje: " + self.mensaje
+
+
