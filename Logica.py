349a39638b7b07c264bb551db4dd707525b16a07
ionatan <ionatan@gmail.com>
2020-09-14 03:59:41 -0300

    Hice algunas mejoras en el tema de saltear pasos innecesarios y pase en limpio el operador EOE

--- Logica.py
+++ Logica.py
@@ -71,7 +71,8 @@ class Parentesis(Operador):
 
     @classmethod
     def aplicarOperador (cls,expresion):
-        """Aplica el operador parentesis a una expresion, asume que es el primer operador aplicado.
+        """
+        Aplica el operador parentesis a una expresion, asume que es el primer operador aplicado.
 
         La funcion toma el texto de la expresion y lo segmenta en funcion de los parentesis mas externos que encuentre colocando
         en la propiedad elementos de la expresion los segmentos de texto que queden fuera de los parentesis y expresiones nuevas para
@@ -86,42 +87,134 @@ class Parentesis(Operador):
         - Que no haya mas de un parentesis sin que quede texto fuera
         
         """        
-        pares = cls.buscarPares(expresion)
-        if pares == -1:
-            return # Se vuelve sin aplicar nada porque hubo un error en la sintaxis de los parentesis.
+        if not cls.saltear(expresion.texto):
+            pares = cls.buscarPares(expresion)
+            if pares == -1:
+                return # Se vuelve sin aplicar nada porque hubo un error en la sintaxis de los parentesis.
+            else:
+                for par in pares:
+                    if par[1] == par[0]+1:
+                        MensajeError(expresion,Parentesis,"En la expresion se encontro un parentesis de apertura y cierre sin contenido.")
+                        return
+                if len(pares) == 1:
+                    if ((pares[0][0] == 0) and (pares[0][1] == len(expresion.texto)-1)):
+                        expresion.texto = expresion.texto[1:-1]
+                        expresion.elementosTemporales = [expresion.texto]
+                        return 
+                remanente = expresion.texto
+                for par in pares:
+                    pre = expresion.texto[len(expresion.texto)-len(remanente):par[0]]
+                    if pre:
+                        expresion.elementos.append(expresion.texto[len(expresion.texto)-len(remanente):par[0]])
+                    contenido = expresion.texto[par[0]+1:par[1]]
+                    expresion.elementosTemporales.append(Expresion(contenido,expresion)) 
+                    remanente = expresion.texto[par[1]+1:]
+                if remanente:
+                    expresion.elementosTemporales.append(remanente)
+                expresion.updateTextoRemanente()
+                if expresion.textoRemanente == "":
+                    MensajeError(expresion,Parentesis,"En la expresion se encontro mas de un parentesis sin ningun tipo de operador que los conecte.")
         else:
-            for par in pares:
-                if par[1] == par[0]+1:
-                    MensajeError(expresion,Parentesis,"En la expresion se encontro un parentesis de apertura y cierre sin contenido.")
-                    return
-            if len(pares) == 1:
-                if ((pares[0][0] == 0) and (pares[0][1] == len(expresion.texto)-1)):
-                    expresion.texto = expresion.texto[1:-1]
-                    expresion.elementosTemporales = [expresion.texto]
-                    return 
-            remanente = expresion.texto
-            for par in pares:
-                pre = expresion.texto[len(expresion.texto)-len(remanente):par[0]]
-                if pre:
-                    expresion.elementos.append(expresion.texto[len(expresion.texto)-len(remanente):par[0]])
-                contenido = expresion.texto[par[0]+1:par[1]]
-                expresion.elementosTemporales.append(Expresion(contenido,expresion)) 
-                remanente = expresion.texto[par[1]+1:]
-            if remanente:
-                expresion.elementosTemporales.append(remanente)
-            expresion.updateTextoRemanente()
-            if expresion.textoRemanente == "":
-                MensajeError(expresion,Parentesis,"En la expresion se encontro mas de un parentesis sin ningun tipo de operador que los conecte.")
+            expresion.elementosTemporales = expresion.texto
+        return
+
+    @classmethod
+    def saltear(cls,texto):
+        if not texto.find(cls.simboloApertura) == -1:
+            return True
+        if not texto.find(cls.simboloCierre) == -1:
+            return True
+        return False
+
+    @classmethod
+    def contarOcurrencias(cls,texto):
+        return True
+
+class EOE(Operador):
+    """
+    Operador generico del tipo EOE que toma una expresion y la segmenta en algo del tipo Expresion Operador Expresion.
+
+    Es una clase completamente estatica ya que no tiene sentido que haya instancias del operado que funciona siempre igual y es 
+    independiente de los objetos (expresiones u proposiciones sobre los que actua)
+    
+    """
+    name = "EOE"
+    asociativo = None
+    prioridad = None
+    simbolo = None
+    
+    @classmethod
+    def contarOcurrencias(cls,texto):
+        "Cuenta cuantas ocurrencias del simbolo que corresponde al operador hay en un texto."
+        numeroDeOcurrencias = 0
+        while not texto.find(cls.simbolo) == -1:
+            texto = texto.replace(cls.simbolo,"",1)
+            numeroDeOcurrencias += 1
+        return numeroDeOcurrencias
+
+    @classmethod
+    def aplicarOperador(cls,expresion):
+        "Verifica que no haya errores de sintaxis en los elementos a procesar (que son una cadena de str y expresiones provenientes de parentesis) y luego segmenta segun corresponda genernado en la expresion que recibe las expresiones y el operador correspondiete."
+        if cls.asociativo:
+            for operador in expresion.operadores:
+                if cls.prioridad == operador.prioridad and not cls.name == operador.name:
+                    if operador.contarOcurrencias(expresion.textoRemanente):
+                        MensajeError(expresion,cls,"En la expresion se encontro un operador del tipo " + cls.simbolo + " junto a otro de tipo " + operador.simbolo + " y es ambiguo cual se debe aplicar primero.")
+                        return
+        else:
+            if cls.contarOcurrencias(expresion.textoRemanente) > 1:
+                MensajeError(expresion,cls,"En la expresion se encontro mas de un operador del tipo " + cls.simbolo + " y es ambiguo cual se debe aplicar primero.")
+                return
+
+        numeroDeElementoConSimbolo = -1
+        for idx, elemento in enumerate(expresion.elementosTemporales):
+            if type(elemento) == str:
+                if elemento.count(cls.simbolo):
+                    numeroDeElementoConSimbolo = idx
+                    break
+        indiceDeOcurrencia = expresion.elementosTemporales[numeroDeElementoConSimbolo].find(cls.simbolo)
+        pre = expresion.elementosTemporales[numeroDeElementoConSimbolo][:indiceDeOcurrencia]
+        post = expresion.elementosTemporales[numeroDeElementoConSimbolo][indiceDeOcurrencia+len(cls.simbolo):]
+        if pre:
+            expresionIzquierda = expresion.elementosTemporales[:numeroDeElementoConSimbolo] + [pre]
+        else:
+            expresionIzquierda = expresion.elementosTemporales[:numeroDeElementoConSimbolo]
+        if post:
+            expresionDerecha = [post] + expresion.elementosTemporales[numeroDeElementoConSimbolo+1:]
+        else:
+            expresionDerecha = expresion.elementosTemporales[numeroDeElementoConSimbolo+1:]
+        if not expresionIzquierda:
+            MensajeError(expresion,cls,"No se encontró una expresion valida delante del operador " + cls.simbolo + ".")
+            return
+        if not expresionDerecha:
+            MensajeError(expresion,cls,"No se encontró una expresion valida detras del operador " + cls.simbolo + ".")
+            return
+        expresion.elementosTemporales = []
+        expresion.subexpresiones = [Expresion.crearsubexpresion(expresionIzquierda,expresion),Expresion.crearsubexpresion(expresionDerecha,expresion)]
+        expresion.operacion = cls
+
+    @classmethod
+    def totext(cls,expresion):
+        return expresion.subexpresiones[0].totext() + cls.simbolo + expresion.subexpresiones[1].totext()
+
+class SiSoloSi (EOE):
+
+    name = "SiSoloSi"
+    equivalencias = [["<=>","<->"]]
+    asociativo = False
+    simbolo = equivalencias[0][0]
+    prioridad = 1
 
 class Expresion:
 
     name = "Expresion"
-    operadores = [Parentesis] #, SiSoloSi, Implica, Proposicion]
+    operadores = [Parentesis , SiSoloSi]#, Implica, Proposicion]
 
     def __init__ (self,texto,padre):
         assert texto
         assert type(texto) == str
         self.texto = texto
+        self.textoRemanente = texto
         self.padre = padre
         self.mensajesError = []
         self.operacion = None
@@ -152,6 +245,16 @@ class Expresion:
         return Expresion(texto,padre)
 
     @classmethod
+    def elementosAtexto(cls,elementosTemporales):
+        texto = ""
+        for item in elementosTemporales:
+            if type(item) == str:
+                texto += item
+            else:
+                texto += item.totext()
+        return texto
+
+    @classmethod
     def reemplazarCaracteres (cls,texto):
         "Reemplaza los caracteres de un texto asumiento que el texto fue ingresado por el usuario y hay diferentes equivalencias para los mismos simbolos"
         for operador in cls.operadores:
@@ -161,6 +264,9 @@ class Expresion:
     @classmethod
     def expresionInicial (cls,texto):
         "Es el punto de entreda al codigo, toma el texto del usuario e inicia el proceso."
+        if not texto:
+            print ("Se debe ingresar un texto para analizar")
+            return
         textoOriginal = texto
         texto = cls.reemplazarCaracteres(texto)
         if not textoOriginal == texto:
@@ -175,21 +281,20 @@ class Expresion:
         return expresion
 
     def totext(self):
-        pass # No me queda claro para que la querria. 
+        raise NotImplementedError # No me queda claro para que la querria. 
         
     def procesar(self):
         for operador in self.operadores:
             if self.continuarvalidacion:
-                operador.aplicarOperador(self)
-                self.updateTextoRemanente()
+                if operador.contarOcurrencias(self.textoRemanente):
+                    operador.aplicarOperador(self)
+                    self.updateTextoRemanente()
 
     def updateTextoRemanente(self):
         self.textoRemanente = ""
         for item in self.elementosTemporales:
             if type(item) == str:
                 self.textoRemanente += item
-    
-        
         
 class MensajeError:
     "Clase que se crea cuando aparece un error en una expresión para explicarlo."
@@ -202,10 +307,14 @@ class MensajeError:
 
     def reportar(self):
         "Genera el texto que explica el error."
-        print ("En el la expresion " + self.contexto.texto + " se encontro un error al procesar un operador de tipo " + self.operador.name + " que reporto el siguiente mensaje: " + self.mensaje)
+        print ("En la expresion " + self.contexto.texto + " se encontro un error al procesar un operador de tipo " + self.operador.name + " que reporto el siguiente mensaje: " + self.mensaje)
 
 
-def test(testeos):
+def test(testeos,indice=0):
+    if indice:
+        expresion = Expresion.expresionInicial(testeos[indice][0])
+        deberiapasar (testeos[indice][1],expresion,testeos[indice][0])
+        return
     for item in testeos:
         expresion = Expresion.expresionInicial(item[0])
         deberiapasar (item[1],expresion,item[0])
@@ -228,7 +337,8 @@ testeos = [
     ["Hola)(Chau",False],
     ["(Hola)p",True], #Por ahora
     ["(Hola)(Chau)",False],
-    ["((Hola)())g",False]
+    ["((Hola)())g",False],
+    ["p<=>q",True]
 ]
 
-test(testeos)
+test(testeos,11)
