2dd8d109ef482e1cf9b42823b5f1df0cb5e907ab
ionatan <ionatan@gmail.com>
2021-02-23 08:40:37 -0300

    Agrego un venv y cosas para probar flask

--- pruebas.py
+++ pruebas.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Chalu puto!'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0")
